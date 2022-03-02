d0312dcb893f479912db1d7cdb77628c1fd35053
Jefferson Dias <jefer.ld@hotmail.com>
2022-03-01 22:42:58 -0300

    input on the radio on userRegister page adjustment

--- src/registers/userRegister.js
+++ src/registers/userRegister.js
@@ -11,10 +11,11 @@ export default function UserRegister(){
         setUser((state)=>{
             if(target.name === "admin"){
                 
-                if(target.checked){
-                    return {...state,[target.name]: "true"}
-                }else{
+                if(target.checked && target.id === "flexRadioDefault1"){
                     return {...state,[target.name]: "false"}
+
+                }else if(target.checked && target.id === "flexRadioDefault2"){
+                    return {...state,[target.name]: "true"}
                 }
 
             }else{
@@ -46,19 +47,20 @@ export default function UserRegister(){
                 <input onChange={changeUser} className="form-control" type='text' name='name' placeholder="Digite o nome" />
                 <input onChange={changeUser} className="form-control" type='email' name='email' placeholder="Digite o email" />
                 <div class="form-check">
-                    <input onChange={changeUser} className="form-check-input" type="radio" name="admin" id="flexRadioDefault1" checked/>
+                    <input onChange={changeUser} className="form-check-input" type="radio" name="admin" id="flexRadioDefault1" />
                     <label className="form-check-label" for="flexRadioDefault1">
                         Usuário
                     </label>
                 </div>
                 <div class="form-check">
-                    <input onChange={changeUser}  className="form-check-input" type="radio" name="admin" id="flexRadioDefault2"/>
+                    <input onChange={changeUser}  className="form-check-input" type="radio" name="admin" id="flexRadioDefault2" />
                     <label className="form-check-label" for="flexRadioDefault2">
                         Administrador
                     </label>
                 </div>
                 <input onChange={changeUser} className="form-control" type='text' name='password' placeholder="Digite a senha" />
                 <button type='submit' className="btn btn-success w-100">Cadastrar</button>
+                
             </form>
         </div>
         </>
